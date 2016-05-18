613a544bb5da9fd479710d511cdd2e6f6d8003a2
Gambit <chernozor@yandex.ru>
2016-05-18 14:10:20 +0300

    release ver.1.0.0

--- app/addons/intarget/controllers/common/addons.post.php
+++ app/addons/intarget/controllers/common/addons.post.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * Plugin Name: uLogin - виджет авторизации через социальные сети
- * Plugin URI:  https://ulogin.ru/
- * Description: uLogin — это инструмент, который позволяет пользователям получить единый доступ к различным
- * Интернет-сервисам без необходимости повторной регистрации, а владельцам сайтов — получить дополнительный
- * приток клиентов из социальных сетей и популярных порталов (Google, Яндекс, Mail.ru, ВКонтакте, Facebook и др.)
- * Version:     2.0.0
- * Author:      uLoginTeam
- * Author URI:  https://ulogin.ru/
- * License:     GPL2
- */
+
 use Tygh\Registry;
 use Tygh\Http;
 
